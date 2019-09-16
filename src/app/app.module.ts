b65ee841540dc04db294b5fa457e21aad31638a1
Mick Evans <codemastermick@gmail.com>
2019-09-16 12:58:04 -0400

    chore: create image gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CakeDisplayComponent } from './cake-display/cake-display.component';
 
+import { Angular2ImageGalleryModule } from 'angular2-image-gallery';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +14,8 @@ import { CakeDisplayComponent } from './cake-display/cake-display.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    Angular2ImageGalleryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
